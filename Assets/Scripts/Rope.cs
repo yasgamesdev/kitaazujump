9ade698c1cc8c3e3668abb6e696668a5fc9dd153
Yas <yasgamesdev@gmail.com>
2018-06-19 07:41:04 +0900

    Add Rope

---
+++ Assets/Scripts/Rope.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rope : MonoBehaviour
+{
+    public bool isRotate = false;
+    public float speed = 1.0f;
+    // Use this for initialization
+    void Start()
+    {
+        isRotate = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isRotate)
+        {
+            float oldRotationX = transform.rotation.x;
+
+            transform.Rotate(new Vector3(360.0f * Time.deltaTime * speed, 0.0f, 0.0f));
+
+            if(transform.rotation.x * oldRotationX < 0)
+            {
+                CheckCollision();
+            }
+        }
+    }
+
+    void CheckCollision()
+    {
+        Debug.Log("Check");
+    }
+}
