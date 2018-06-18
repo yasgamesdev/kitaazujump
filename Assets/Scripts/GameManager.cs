4e8223c3a36cdd902760863222cc872f2cb0892c
Yas <yasgamesdev@gmail.com>
2018-06-19 08:54:41 +0900

    Add Kitaazuchan

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameState state { get; private set; } = GameState.Start;
+
+    public Kitaazuchan kitaazuchan;
+    public Rope rope;
+
+    float buttonDownTime = 0.0f;
+    // Use this for initialization
+    void Start()
+    {
+        state = GameState.Play;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(state == GameState.Play && Input.GetMouseButton(0))
+        {
+            buttonDownTime += Time.deltaTime;
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            kitaazuchan.Jump(buttonDownTime);
+            buttonDownTime = 0.0f;
+        }
+    }
+}
+
+public enum GameState
+{
+    Start,
+    Play,
+    End
+}
---
+++ Assets/Scripts/Kitaazuchan.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kitaazuchan : MonoBehaviour {
+    Rigidbody rigid;
+    public float force;
+	// Use this for initialization
+	void Start () {
+        rigid = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Jump(float buttonDownTime)
+    {
+        rigid.AddForce(new Vector3(0.0f, force * buttonDownTime, 0.0f));
+    }
+}
